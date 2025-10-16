20a11ddb9627ca9b1d93a699b87e85774d3d1876
KrrishSR4 <krishmishra4444@gmail.com>
2025-10-16 23:35:50 +0530

    changes commited

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Hello Krrish How Are You Doing Today....!!!!!?
+            Helloooooooooo Krrish How Are You Doing Today....!!!!!?
           </li>
         </ol>
 
